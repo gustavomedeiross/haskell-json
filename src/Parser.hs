f756c334acb4d5ac6e391e60ac53225fddb76770
Gustavo Mendes de Medeiros <gustavomendes.medeiros@gmail.com>
2022-02-02 13:27:11 -0300

    Add unit tests

--- src/Parser.hs
+++ src/Parser.hs
@@ -1,24 +1,26 @@
 {-# LANGUAGE LambdaCase #-}
 
-module Main where
+module Parser where
 
+import           Control.Applicative
 import           Data.Char
-import Control.Applicative
 
 newtype Parser a =
   Parser
     { parse :: String -> Maybe (String, a)
     }
 
-instance Functor Parser where
+instance Functor Parser
   -- fmap :: (a -> b) -> Parser a -> Parser b
+                                              where
   fmap f parserA =
     Parser $ \input -> do
       (input', a) <- parse parserA input
       Just (input', f a)
 
-instance Applicative Parser where
+instance Applicative Parser
   -- pure :: a -> Parser a
+                           where
   pure a = Parser $ \input -> Just (input, a)
   -- <*> :: Parser (a -> b) -> Parser a -> Parser b
   parserA <*> parserB =
@@ -27,37 +29,45 @@ instance Applicative Parser where
       (input'', a) <- parse parserB input'
       Just (input'', f a)
 
-instance Alternative Parser where
+instance Alternative Parser
   -- empty :: Parser a
+                       where
   empty = Parser (const Nothing)
   -- <|> :: Parser a -> Parser a -> Parser a
-  parserA <|> parserB = Parser $ \input ->
-    case parse parserA input of
-      Just (input', a) -> Just (input', a)
-      Nothing -> parse parserB input
+  parserA <|> parserB =
+    Parser $ \input ->
+      case parse parserA input of
+        Just (input', a) -> Just (input', a)
+        Nothing          -> parse parserB input
 
-instance Monad Parser where
+instance Monad Parser
   -- >>= :: Parser a -> (a -> Parser b) -> Parser b
-  parserA >>= f = Parser $ \input -> do
-     (input', a) <- parse parserA input
-     parse (f a) input'
+                                                    where
+  parserA >>= f =
+    Parser $ \input -> do
+      (input', a) <- parse parserA input
+      parse (f a) input'
 
 char :: Char -> Parser Char
-char x = Parser $ \case
-    y:ys | y == x -> Just (ys, y)
+char x =
+  Parser $ \case
+    y:ys
+      | y == x -> Just (ys, y)
     _ -> Nothing
 
 string :: String -> Parser String
 string = traverse char
 
 satisfies :: (Char -> Bool) -> Parser Char
-satisfies f = Parser $ \case
-    y:ys | f y -> Just (ys, y)
+satisfies f =
+  Parser $ \case
+    y:ys
+      | f y -> Just (ys, y)
     _ -> Nothing
 
 sepBy :: Parser a -> Parser b -> Parser [b]
 sepBy sep element = (:) <$> element <*> many (sep *> element) <|> pure []
-  
+
 digit :: Parser Char
 digit = satisfies isDigit
 
@@ -78,14 +88,13 @@ normalChar = satisfies ((&&) <$> (/= '"') <*> (/= '\\'))
 
 -- TODO: handle unicode escape (\u)
 escapeChar :: Parser Char
-escapeChar = ('"' <$ string "\\\"") <|>
-             ('\\' <$ string "\\\\") <|>
-             ('/' <$ string "\\/") <|>
-             ('\b' <$ string "\\b") <|>
-             ('\f' <$ string "\\f") <|>
-             ('\n' <$ string "\\n") <|>
-             ('\r' <$ string "\\r") <|>
-             ('\t' <$ string "\\t")
+escapeChar =
+  ('"' <$ string "\\\"") <|> ('\\' <$ string "\\\\") <|> ('/' <$ string "\\/") <|>
+  ('\b' <$ string "\\b") <|>
+  ('\f' <$ string "\\f") <|>
+  ('\n' <$ string "\\n") <|>
+  ('\r' <$ string "\\r") <|>
+  ('\t' <$ string "\\t")
 
 stringLiteral :: Parser String
 stringLiteral = char '"' *> many (normalChar <|> escapeChar) <* char '"'
@@ -94,8 +103,8 @@ pair :: Parser a -> Parser b -> Parser (a, b)
 pair = liftA2 (,)
 
 -- Json
-
-data JsonValue = JsonNull
+data JsonValue
+  = JsonNull
   | JsonBool Bool
   | JsonString String
   | JsonInt Int
@@ -105,7 +114,8 @@ data JsonValue = JsonNull
   deriving (Show, Eq)
 
 json :: Parser JsonValue
-json = jsonNull <|> jsonBool <|> jsonInt <|> jsonString <|> jsonArray <|> jsonObject
+json =
+  jsonNull <|> jsonBool <|> jsonInt <|> jsonString <|> jsonArray <|> jsonObject
 
 jsonNull :: Parser JsonValue
 jsonNull = JsonNull <$ string "null"
@@ -130,9 +140,6 @@ jsonArray = fmap JsonArray $ char '[' *> ws *> arrayValues <* ws <* char ']'
 
 jsonObject :: Parser JsonValue
 jsonObject = fmap JsonObject $ char '{' *> ws *> assoc <* ws <* char '}'
- where
-   assoc = sepBy comma $ pair (stringLiteral <* ws <* char ':' <* ws) json
-   comma = ws *> char ',' <* ws
-
-main :: IO ()
-main = putStrLn "Hello, Haskell!"
+  where
+    assoc = sepBy comma $ pair (stringLiteral <* ws <* char ':' <* ws) json
+    comma = ws *> char ',' <* ws
---
+++ test/Spec.hs
@@ -0,0 +1,58 @@
+module Main where
+
+import           Parser
+import           Test.Tasty
+import           Test.Tasty.HUnit
+
+main :: IO ()
+main = defaultMain tests
+
+tests :: TestTree
+tests = testGroup "Tests" [parserTests, jsonParserTests]
+
+parserTests =
+  testGroup
+    "Parser Tests"
+    [ testCase "Can parse a char" $ parse (char 'a') "abc" @?= Just ("bc", 'a')
+    , testCase "Parsing a char can fail" $ parse (char 'a') "bc" @?= Nothing
+    , testCase "Can parse a string" $
+      parse (string "hello") "hello world" @?= Just (" world", "hello")
+    , testCase "Can parse a positive integer" $
+      parse integer "123" @?= Just ("", 123)
+    , testCase "Can parse a negative integer" $
+      parse integer "-123" @?= Just ("", -123)
+    ]
+
+jsonParserTests =
+  testGroup
+    "Json Parser Tests"
+    [
+      testCase "Can parse null" $ parse json "null" @?= Just ("", JsonNull)
+
+    , testCase "Can parse the boolean true" $
+      parse json "true" @?= Just ("", JsonBool True)
+
+    , testCase "Can parse the boolean false" $
+      parse json "false" @?= Just ("", JsonBool False)
+
+    , testCase "Can parse a string literal" $
+      parse json "\"a string literal\"" @?=
+      Just ("", JsonString "a string literal")
+
+    , testCase "Can parse a json int" $
+      parse json "1234" @?= Just ("", JsonInt 1234)
+
+    , testCase "Can parse a json array" $
+      parse json "[1, true, \"hello\"]" @?=
+      Just ("", JsonArray [JsonInt 1, JsonBool True, JsonString "hello"])
+
+    , testCase "Can parse a json object" $
+      parse json "{ \"hello\": \"world\", \"something\": 123 }" @?=
+      Just
+        ( ""
+        , JsonObject [("hello", JsonString "world"), ("something", JsonInt 123)])
+
+    , testCase "Can parse a json object with an array" $
+      parse json "{ \"array\": [1, 2] }" @?=
+      Just ("", JsonObject [("array", JsonArray [JsonInt 1, JsonInt 2])])
+    ]
